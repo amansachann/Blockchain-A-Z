a3662ad9afe4ce3b40700c9b8dfb5d969a87dcb8
Aman Sachan <aman@Amans-MacBook-Pro.local>
2024-09-11 19:25:56 +0530

    starting Module-1 - Creating a Blockchain

---
+++ Module-1/blockchain.py
@@ -0,0 +1,15 @@
+# Module - 1 - Create a Blockchain
+
+# To be installed 
+# - Flask
+# - Postman
+
+#  Importing the libraries
+import datetime
+import hashlib
+import json
+from flask import Flask, jsonify
+
+# Part-1 - Building a blockchain
+
+# Part-2 - Mining our blockchain
